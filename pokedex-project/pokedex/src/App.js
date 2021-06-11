34ce8dc4c28033fbf72b187c419a9708dd89e016
Ben Harter-Murphy <benhartermurphy@gmail.com>
2021-06-11 15:40:40 -0400

    finsihed poke game

--- pokedex-project/pokedex/src/App.js
+++ pokedex-project/pokedex/src/App.js
@@ -1,16 +1,12 @@
 import React from 'react'
-import PokeCard from './PokeCard'
+// import Pokedex from './Pokedex'
 import './App.css';
+import Pokegame from './Pokegame';
 
 function App() {
   return (
     <div className="App">
-      <PokeCard 
-        id={4}
-        name="Charmander"
-        type="Fire"
-        exp={62}
-      />
+      <Pokegame />
     </div>
   );
 }
